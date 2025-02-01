3a1c07e7235f37be5c0ff6ca09a9dda667b3ebec
casperjr <hyunjw0515@gmail.com>
2025-02-01 15:49:03 +0900

    [FEAT]: 앱 배경 색상들 지정(#10)

--- app/src/main/java/com/pickpoint/pickpoint/ui/theme/Color.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.pickpoint.pickpoint.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//홈화면 버튼, 각종 컨테이너 (설정, bottom sheet 등)
+val PrototypePrimaryColor = Color(0xFFEEEEEE)
+//홈화면 버튼, 각종 컨테이너 (설정, bottom sheet 등) 에서의 텍스트, 아이콘 등의 색상
+val PrototypeOnPrimaryColor = Color(0xFF333333)
+//기본 배경화면 색상
+val PrototypeBackgroundColor = Color(0xFFFFFFFF)
+//Top App Bar 텍스트 색상
+val ProtorypeSecondaryColor = Color(0xFF555555)
+//HorizontalDivider 색상
+val PrototypeTertiaryColor = Color(0xFFD9D9D9)
