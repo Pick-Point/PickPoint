4bbf77b2dea2d1f9e39ab235fbb0c79c8f9b48f9
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-01 16:26:34 +0900

    [feat]: MainActivity 에서 viewModel 생성 후 HomeScreen 에 넘기는 기능 추가 (#15)

--- app/src/main/java/com/pickpoint/pickpoint/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/home/screen/HomeScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import com.pickpoint.pickpoint.ui.home.viewmodel.HomeViewModel
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(
+    viewModel: HomeViewModel,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = Modifier.fillMaxSize()) {
         Text(text = "HomeScreen", fontSize = 30.sp)
     }
