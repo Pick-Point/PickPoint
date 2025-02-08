219a60ea10708bd7518c41d9b9ce6166ce72f470
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-08 15:52:09 +0900

    [feat]: remember , changed 함수를 viewmodel 로 이동 (#23)

--- app/src/main/java/com/pickpoint/pickpoint/ui/whattodo/screen/WhatToDoScreen.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/whattodo/screen/WhatToDoScreen.kt
@@ -1,67 +1,77 @@
 package com.pickpoint.pickpoint.ui.whattodo.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pickpoint.pickpoint.ui.common.component.MainTopAppBar
 import com.pickpoint.pickpoint.ui.common.component.NumberSettingComponent
 import com.pickpoint.pickpoint.ui.common.component.ResultsComponent
+import com.pickpoint.pickpoint.ui.whattodo.viewmodel.WhatToDoViewmodel
 
 @Composable
-fun WhatToDoScreen() {
-    var count by remember { mutableIntStateOf(4) }
-    val resultList = remember { mutableStateListOf<String>() }
-    repeat(count) {
-        resultList.add("")
-    }
+fun WhatToDoScreen(
+    viewmodel: WhatToDoViewmodel = viewModel()
+) {
+
+    val count by viewmodel.count.collectAsState()
+    val resultList by viewmodel.resultList.collectAsState()
 
     Scaffold(
         topBar = { MainTopAppBar(title = "What to do") },
     ) { innerPadding ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            NumberSettingComponent(
-                modifier = Modifier
-                    .padding(top = 30.dp)
-                    .padding(horizontal = 20.dp),
-                label = "Total Points",
-                currentNumber = count,
-                onPlusButtonClick = {
-                    if (count < 10) {
-                        count++
-                        resultList.add("")
-                    }
-                },
-                onMinusButtonClick = {
-                    if (count > 1) {
-                        resultList.removeAt(--count)
-                    }
-                }
-            )
+            Column {
 
-            ResultsComponent(
+                NumberSettingComponent(
+                    modifier = Modifier
+                        .padding(top = 30.dp)
+                        .padding(horizontal = 20.dp),
+                    label = "Total Points",
+                    currentNumber = count,
+                    onPlusButtonClick = {
+                        if (count < 10)
+                            viewmodel.onPlusButtonClick()
+                    },
+                    onMinusButtonClick = {
+                        if (count > 1)
+                            viewmodel.onMinusButtonClick()
+                    }
+                )
+
+                ResultsComponent(
+                    modifier = Modifier
+                        .padding(top = 46.dp),
+                    title = "Results",
+                    count = count,
+                    resultList = resultList,
+                    onResultChanged = { index, result ->
+                        viewmodel.updateResultIndex(index, result)
+                    }
+                )
+            }
+
+            Row(
                 modifier = Modifier
-                    .padding(top = 46.dp),
-                title = "Results",
-                count = count,
-                resultList = resultList,
-                onResultChanged = { index, result ->
-                    resultList[index] = result
-                }
-            )
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+            }
         }
     }
 }
---
+++ app/src/main/java/com/pickpoint/pickpoint/ui/whattodo/viewmodel/WhatToDoViewmodel.kt
@@ -0,0 +1,38 @@
+package com.pickpoint.pickpoint.ui.whattodo.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class WhatToDoViewmodel: ViewModel() {
+
+    private val _count : MutableStateFlow<Int> = MutableStateFlow(4)
+    val count : StateFlow<Int> = _count.asStateFlow()
+
+    private val _resultList : MutableStateFlow<List<String>> = MutableStateFlow(mutableListOf(
+        "", "", "", ""
+    ))
+    val resultList : StateFlow<List<String>> = _resultList.asStateFlow()
+
+
+
+    fun onPlusButtonClick() {
+        if (_count.value < 10) {
+            _count.value++
+            _resultList.value = _resultList.value.toMutableList().apply { add("") }
+        }
+    }
+
+    fun onMinusButtonClick() {
+        if (_count.value > 1) {
+            _count.value--
+            _resultList.value = _resultList.value.toMutableList().apply { removeAt(_count.value) }
+        }
+    }
+
+    fun updateResultIndex(index: Int, result: String) {
+        _resultList.value = _resultList.value.toMutableList().apply { set(index, result) }
+    }
+
+}
