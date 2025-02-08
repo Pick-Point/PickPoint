60e0ab45af11ecf358d850a0eddbec215dd96427
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-09 00:40:22 +0900

    [feat]: 파라미터의 수 만큼 컬러들을 랜덤으로 가져오는 함수 구현  (#23)

--- app/src/test/java/com/pickpoint/pickpoint/ExampleUnitTest.kt
+++ app/src/test/java/com/pickpoint/pickpoint/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.pickpoint.pickpoint
 
+import com.pickpoint.pickpoint.ui.common.util.getRandomElements
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,26 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `랜덤 개수 반환 테스트`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val randomList = list.getRandomElements(3)
+        assertEquals(3, randomList.size)
+    }
+
+    @Test
+    fun `최대 개수가 넘는 숫자를 인자로 넘길 때 셔플된 리스트가 반환되는지 확인`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val randomList = list.getRandomElements(10)
+        assertEquals(5, randomList.size)
+    }
+
+    @Test
+    fun `사이즈의 개수만큼 인자로 넘길 때 셔플된 리스트가 반환되는지 확인`() {
+        val list = listOf(1, 2, 3, 4, 5)
+        val randomList = list.getRandomElements(5)
+        assertEquals(5, randomList.size)
     }
 }
