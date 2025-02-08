6fa7f96cb59f5f5b4b283e1e38246ea13375e5cf
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-08 14:59:05 +0900

    [refactor]: SettingComponent 그림자 수정 (#23)

--- app/src/main/java/com/pickpoint/pickpoint/ui/common/component/SettingComponent.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/common/component/SettingComponent.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -68,6 +71,9 @@ fun SettingComponent(
                     .align(Alignment.CenterVertically)
             )
         }
+        Card(
+            elevation = CardDefaults.cardElevation(6.dp)
+        ) {
         Column(
             modifier = Modifier
                 .shadow(elevation = 4.dp)
@@ -111,7 +117,7 @@ fun SettingComponent(
                     )
                 }
             }
-        }
+        }}
     }
 }
 
