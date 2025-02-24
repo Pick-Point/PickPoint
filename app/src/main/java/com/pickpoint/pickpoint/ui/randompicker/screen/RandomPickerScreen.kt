6384b263a318b9007acb90fe13294acd62473063
Chae Minji <162430621+alswlekk@users.noreply.github.com>
2025-02-24 10:48:17 +0900

    RandomPickerScreen, TeamMakerScreen 화면 구현

--- app/src/main/java/com/pickpoint/pickpoint/ui/randompicker/screen/RandomPickerScreen.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/randompicker/screen/RandomPickerScreen.kt
@@ -1,15 +1,53 @@
 package com.pickpoint.pickpoint.ui.randompicker.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import com.pickpoint.pickpoint.ui.common.component.MainTopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun RandomPickerScreen(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "RandomPickerScreen", fontSize = 30.sp)
+    Column(modifier = Modifier.fillMaxSize(),
+    ) {
+        Scaffold(
+            topBar = {
+                MainTopAppBar(
+                    title = "Random Picker",
+                    modifier = modifier,
+                    onNavigationClick = { /*뒤로가기 동작*/ },
+                )
+            }
+        ) {
+                paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .background(Color.White),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Tap to start!", fontSize = 28.sp,
+                    modifier = Modifier
+                        .background(Color.White)
+
+                )
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun RandomPickerScreenPreview() {
+    RandomPickerScreen()
 }
