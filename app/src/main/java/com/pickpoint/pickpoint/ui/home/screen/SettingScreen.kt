432240d39c058b3315c08efd093a77cf81b264b2
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-03-01 18:23:13 +0900

    [chore]: SettingScreen 의 문자열들을 stringResource 로 대체 (#37)

--- app/src/main/java/com/pickpoint/pickpoint/ui/home/screen/SettingScreen.kt
+++ app/src/main/java/com/pickpoint/pickpoint/ui/home/screen/SettingScreen.kt
@@ -12,11 +12,13 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.pickpoint.pickpoint.R
 import com.pickpoint.pickpoint.ui.common.component.SecondaryTopAppBar
 import com.pickpoint.pickpoint.ui.common.component.SettingComponent
 import com.pickpoint.pickpoint.ui.home.viewmodel.SettingViewModel
@@ -25,7 +27,6 @@ import com.pickpoint.pickpoint.ui.model.setting.PreferencesSetting
 import com.pickpoint.pickpoint.ui.model.setting.ThemeSetting
 import com.pickpoint.pickpoint.ui.common.component.ResetConfirmButton
 import com.pickpoint.pickpoint.ui.theme.AppTheme
-import com.pickpoint.pickpoint.ui.theme.LightPrototypeBackgroundColor
 import com.pickpoint.pickpoint.ui.theme.PickPointTheme
 
 @Composable
@@ -35,9 +36,9 @@ fun SettingsScreen(
     onNavigateBack: () -> Unit = {}
 ) {
 
-    val themeIndex = viewModel!!.themeSettingIndex.collectAsState()
-    val languageIndex = viewModel.languageSettingIndex.collectAsState()
-    val preferencesIndex = viewModel.preferencesSettingIndex.collectAsState()
+    val themeIndex = viewModel!!.themeSettingIndex.collectAsStateWithLifecycle()
+    val languageIndex = viewModel.languageSettingIndex.collectAsStateWithLifecycle()
+    val preferencesIndex = viewModel.preferencesSettingIndex.collectAsStateWithLifecycle()
 
     Surface(
         modifier = modifier
@@ -52,24 +53,23 @@ fun SettingsScreen(
                 .fillMaxSize()
         ) {
             SecondaryTopAppBar(
-                title = "Settings",
+                title = stringResource(id = R.string.settings),
                 onNavigationClick = onNavigateBack
             )
             Spacer(modifier = Modifier.padding(15.dp))
 
-            //테마 설정
+            // 테마 설정
             SettingComponent(
-                title = "Theme",
+                title = stringResource(id = R.string.theme),
                 settingRes = ThemeSetting.entries.map { it.res },
-                //checkedIndex = 0,
                 checkedIndex = themeIndex.value,
                 onClick = { viewModel.updateThemeSettingIndex(it) }
             )
             Spacer(modifier = Modifier.padding(15.dp))
 
-            //언어 설정
+            // 언어 설정
             SettingComponent(
-                title = "Language",
+                title = stringResource(id = R.string.language),
                 settingRes = LanguageSetting.entries.map { it.res },
                 checkedIndex = languageIndex.value,
                 onClick = { viewModel.updateLanguageSettingIndex(it) }
@@ -78,7 +78,7 @@ fun SettingsScreen(
 
             // 추가 설정
             SettingComponent(
-                title = "Preferences",
+                title = stringResource(id = R.string.preferences),
                 settingRes = PreferencesSetting.entries.map { it.res },
                 checkedIndex = preferencesIndex.value,
                 onClick = { viewModel.updatePreferencesSettingIndex(it) }
